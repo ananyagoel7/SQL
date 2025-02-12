9e6d0667e28ad7b53ea34c29e79b8e60f980f98a
Ananya <ananyagoel.brac@gmail.com>
2025-02-12 11:29:09 +0530

    Completed Queries

--- Blinkit store.sql
+++ Blinkit store.sql
@@ -33,4 +33,18 @@ from blinkit_data group by Outlet_Location_Type order by Outlet_Location_Type;
 select Outlet_Establishment_Year, round(sum(Total_Sales), 2) as Total_Sales from blinkit_data
 group by Outlet_Establishment_Year order by Outlet_Establishment_Year;
 
+-- % of sales by outlet size --
+select Outlet_Size, round(sum(Total_Sales), 2) AS Total_Sales,
+round((sum(Total_Sales) * 100.0 / (select sum(Total_Sales) from blinkit_data)), 2) AS Sales_Percentage
+from blinkit_data group by Outlet_Size order by Total_Sales DESC;
+
+-- Sales by outlet location --
+select Outlet_Location_Type, round(sum(Total_Sales) , 2) AS Total_Sales from blinkit_data
+group by Outlet_Location_Type order by Total_Sales DESC;
+
+-- All metrics by outlet type --
+select Outlet_Type, round(sum(Total_Sales), 2) AS Total_Sales, round(avg(Total_Sales), 0) AS Avg_Sales,
+count(*) AS No_Of_Items, round(avg(Rating), 2) AS Avg_Rating, round(avg(Item_Visibility), 2) AS Item_Visibility
+from blinkit_data group by Outlet_Type order by Total_Sales DESC;
+
 
